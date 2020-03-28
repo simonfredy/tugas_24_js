734816d9470a7cc7fa50141ced7911f8387bcc40
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-28 20:04:20 +0700

    Tugas 24 JS

---
+++ tugas24.js
@@ -0,0 +1,12 @@
+function tugasDuaPuluhEmpat(){
+    var arrDeret = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    console.log("Sebelumnya", arrDeret);
+    
+    var arrAsc = arrDeret.sort();
+    console.log("Ascending", arrAsc);
+
+    var arrDesc = arrDeret.reverse();
+    console.log("Descending", arrDesc);
+    
+}
+tugasDuaPuluhEmpat();
